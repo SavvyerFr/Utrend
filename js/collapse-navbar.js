b59df45c72fd7e636128825f0b500d01de3e9df9
Dmytro <fokshei.d@gmail.com>
2016-11-20 13:05:25 +0000

    1

---
+++ js/collapse-navbar.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+$(function() {
+  $(document).on('click','.navbar-collapse.in',function(e) {
+      if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+          $(this).collapse('hide');
+      }
+  });
+});
